be4a46f5b8919ae18340e2507a8a692db7ee4746
LMing0923 <benson.net82@gmail.com>
2024-06-29 01:08:24 +0800

    Initial commit

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// firebaseConfig.js
+const firebaseConfig = {
+    apiKey: "AIzaSyBh_D-Si856AUMANrMHcZbGn7fAhftjMvQ",
+    authDomain: "time-controler.firebaseapp.com",
+    projectId: "time-controler",
+    storageBucket: "time-controler.appspot.com",
+    messagingSenderId: "510270938510",
+    appId: "1:510270938510:web:57c604b8aec63619ef98df",
+    measurementId: "G-LFBT4E7CPF"
+  }
+
+export default firebaseConfig;
